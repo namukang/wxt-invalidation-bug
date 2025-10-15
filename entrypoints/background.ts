75b6e1b77e09e97f3cceb76bbe068e6a11703b5f
Namu <535248+namukang@users.noreply.github.com>
2025-10-15 00:00:31 -0700

    feat: add proof of concept for invalidation bug

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,16 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+
+  browser.action.onClicked.addListener(async (tab: Browser.tabs.Tab) => {
+    const tabId = tab.id!;
+    injectContentScript(tabId);
+    injectContentScript(tabId);
+  });
 });
+
+async function injectContentScript(tabId: number) {
+  await browser.scripting.executeScript({
+    target: { tabId },
+    files: ["content-scripts/content.js"],
+  });
+}
--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,14 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  // Set "registration" to runtime so this file isn't listed in manifest
+  registration: "runtime",
+  matches: ["<all_urls>"],
+  main(ctx) {
+    const id = Math.random().toString(36).slice(2);
+
+    console.log(`[${id}] hello`);
+
+    ctx.onInvalidated(() => {
+      console.log(`[${id}] invalidated`);
+    });
   },
 });
